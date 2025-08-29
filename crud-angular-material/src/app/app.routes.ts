723447223bfc11452ff8ad026e3543d132fa774e
lucascorbera <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>
2025-08-29 00:08:09 -0300

    Material disgn new

--- crud-angular-material/src/app/app.routes.ts
+++ crud-angular-material/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Cadastro } from './components/cadastro/cadastro'
+import { Consulta } from './components/consulta/consulta'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'cadastro', component: Cadastro },
+  { path: 'consulta', component: Consulta }
+];
