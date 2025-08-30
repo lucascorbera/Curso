525f7aedf1a68fda3d86d47828abff25fa734970
lucascorbera <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>
2025-08-29 23:18:31 -0300

    curso novo

--- crud-angular-material/src/app/components/cadastro/cadastro.ts
+++ crud-angular-material/src/app/components/cadastro/cadastro.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import { Cliente } from './Cliente';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    NgxMaskDirective
+  ],
   templateUrl: './cadastro.html',
-  styleUrl: './cadastro.scss'
+  styleUrls: ['./cadastro.scss']
 })
 export class Cadastro {
 
+  cliente: Cliente = Cliente.newCliente();
+
+
+  Salvar() {
+    console.log(this.cliente);
+  }
 }
