83a8c47d42b8c4702ecd68d4ff3dcd7a7cab47c4
lucascorbera <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>
2025-09-02 00:48:58 -0300

    boa noite e

--- crud-angular-material/src/app/app.ts
+++ crud-angular-material/src/app/app.ts
@@ -2,9 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink} from '@angular/router';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import {MatDividerModule} from '@angular/material/divider';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule, MatButtonModule, MatDividerModule ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
