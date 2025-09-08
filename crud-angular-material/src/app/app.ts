7e34f7f3fcc8dceaa3456bd64c8ca872a9d254ce
tbf00978 <tbf00978@tecban.com>
2025-09-08 20:03:34 -0300

    menu laterial

--- crud-angular-material/src/app/app.ts
+++ crud-angular-material/src/app/app.ts
@@ -4,12 +4,19 @@ import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
 import {MatDividerModule} from '@angular/material/divider';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {FormsModule} from '@angular/forms';
+import { MatMenuModule} from '@angular/material/menu'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule, MatButtonModule, MatDividerModule ],
+  imports: [MatMenuModule,MatListModule, MatSidenavModule,  FormsModule, MatButtonModule,RouterOutlet, RouterLink, MatToolbarModule, MatIconModule, MatButtonModule, MatDividerModule ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('crud-angular-material');
+    events: string[] = [];
+    opened?: boolean;
+    protected readonly title = signal('crud-angular-material');
+     shouldRun = true;
 }
